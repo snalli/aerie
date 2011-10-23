b210c6c71f2f87b588b9884db0283cf2f98a1332
Haris Volos <hvolos@cs.wisc.edu>
2011-10-23 14:26:13 -0500

    namespace interactive test

---
+++ libfs/test/interactive/namespace/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#ROOT=/home/hvolos/workspace/stamnos/libfs/
+#ROOT=/home/hvolos/workspace/muses/stamnos/libfs/test/interactive/lock_tester
+#ROOT=../../../
+
+DEBUG_LEVEL=5
+
+#cd $ROOT
+
+if [ "$1" = "-r" ]
+then
+rm chunkstore.locks
+rm *.pheap
+fi
+if [ "$1" = "-d" ]
+then
+GDB='gdb --args '
+else
+WAITKEY='read'
+fi
+pkill -9 fsclient
+pkill -9 fsserver
+gnome-terminal --geometry=140x15+0+0 --tab -e "./build/src/server/fsserver -p 10000 -d $DEBUG_LEVEL" &
+#./build/test/interactive/lock_tester/lock_tester -h 10000 -i 1 -d $DEBUG_LEVEL
+gnome-terminal --geometry=140x15+0-400 -x bash -c "$GDB./build/test/interactive/namespace/namespace -h 10000 -i 1 -t C1 -d $DEBUG_LEVEL; $WAITKEY" &
+gnome-terminal --geometry=140x15+0-100 -x bash -c "$GDB./build/test/interactive/namespace/namespace -h 10000 -i 2 -t C2 -d $DEBUG_LEVEL; $WAITKEY"
+#pkill -9 fsclient
+#pkill -9 fsserver
---
+++ libfs/test/interactive/namespace/test.cc
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "rpc/rpc.h"
+#include "common/lock_protocol.h"
+#include "client/client_i.h"
+#include "client/libfs.h"
+#include "client/hlckmgr.h"
+
+using namespace client;
+
+static lock_protocol::LockId root = 1;
+static lock_protocol::LockId a = 2;
+static lock_protocol::LockId b = 3;
+static lock_protocol::LockId c = 4;
+static lock_protocol::LockId d = 5;
+static lock_protocol::LockId e = 6;
+static lock_protocol::LockId f = 7;
+static lock_protocol::LockId g = 8;
+
+
+void test11(char* tag)
+{
+	printf("%s\n", __FUNCTION__);
+	libfs_mkfs("/superblock/A", "mfs", 0);
+	libfs_mount("/superblock/A", "/home/hvolos", "mfs", 0);
+	libfs_mkdir("/home/hvolos/dir", 0);
+	libfs_mkdir("/home/hvolos/dir/test", 0);
+	
+	
+	//libfs_open("/etc/hvolos/test", 0);
+	//libfs_open("/home/hvolos/test", 0);
+	//libfs_open("/home/hvolos/dir/test", O_CREATE);
+	//libfs_open("/home/hvolos/file", O_CREATE);
+
+	printf("%s: DONE\n", __FUNCTION__);
+	sleep(1000);
+}
+
+void test12(char* tag)
+{
+	printf("%s\n", __FUNCTION__);
+	sleep(1);
+	libfs_mount("/superblock/A", "/home/hvolos", "mfs", 0);
+	//libfs_mkdir("/home/hvolos/dir", 0);
+	printf("%s: DONE\n", __FUNCTION__);
+}
+
+
+void test(char* tag) {
+	if (strcmp(tag, "C1") == 0) {
+		test11(tag);
+	} else {
+		test12(tag);
+	}
+}
