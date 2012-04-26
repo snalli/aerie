c98cc8d91a7f2db73907f5f8f021d4dbccf1167a
Sankar <sankarp@cs.wisc.edu>
2012-04-26 17:19:37 -0500

    fixed profiler macros to avoid collisions with local variables

--- libfs/src/common/prof.h
+++ libfs/src/common/prof.h
@@ -3,17 +3,17 @@
 
 #include "common/hrtime.h"
 
-#define PROFILER_PREAMBLE                   \
-	hrtime_t      start = hrtime_cycles();   \
-	hrtime_t      stop;                      \
-	int           startln = __LINE__; 
+#define PROFILER_PREAMBLE                          \
+	hrtime_t      __start = hrtime_cycles();   \
+	hrtime_t      __stop;                      \
+	int           __startln = __LINE__; 
 
 #define __PROFILER_SAMPLE        \
 	hrtime_barrier();             \
-	stop = hrtime_cycles();    \
-	printf("%s:%s:%d-%d: %llu\n", __FILE__, __FUNCTION__, startln, __LINE__, stop-start); \
-	start = hrtime_cycles();   \
-	startln = __LINE__;
+	__stop = hrtime_cycles();    \
+	printf("%s:%s:%d-%d: %llu\n", __FILE__, __FUNCTION__, __startln, __LINE__, __stop-__start); \
+	__start = hrtime_cycles();   \
+	__startln = __LINE__;
 
 #define PROFILER_SAMPLE
 
