721e537f5d7cf5d3d36ac8f692803df6414cc70c
Haris Volos <hvolos@cs.wisc.edu>
2012-05-01 16:27:21 -0500

    checked in integration tests tiggering unlink bug we saw with filebench

--- libfs/test/integration/mfs/file.py
+++ libfs/test/integration/mfs/file.py
@@ -9,6 +9,21 @@ import os
 
 def addIntegrationTests(env, parent_dir, testProgram, serverProgram):
     #
+    # FILE TESTS (SINGLE CLIENTS)
+    #
+
+    env.addIntegrationTest(testfw.integration_test.IntegrationTest(
+        name = 'MFSFile:TestCreateUnlink',
+        init_script = os.path.join(parent_dir, 'test/integration/mfs/init.sh'),
+        testfw = testProgram, server = serverProgram,
+        clients = { 
+            'C1': ( testProgram, [('T1', 'MFSFile:TestCreateUnlink')]),
+        },
+        rendezvous = []
+    ))
+    
+    
+    #
     # FILE TESTS (CONCURRENT CLIENTS)
     #
 
@@ -43,6 +58,31 @@ def addIntegrationTests(env, parent_dir, testProgram, serverProgram):
         clients = { 
             'C1': ( testProgram, [('T1', 'MFSFile:TestCreate3')]),
             'C2': ( testProgram, [('T1', 'MFSFile:TestRead3')])
+        },
+        rendezvous = [('C1:T1:E2:block', 'C2:T1:E1:block'), 
+                      ('C1:T1:E3:block', 'C2:T1:E3:block')]
+    ))
+
+
+    env.addIntegrationTest(testfw.integration_test.IntegrationTest(
+        name = 'MFSFile:TestCreateUnlinkConcurrent',
+        init_script = os.path.join(parent_dir, 'test/integration/mfs/init.sh'),
+        testfw = testProgram, server = serverProgram,
+        clients = { 
+            'C1': ( testProgram, [('T1', 'MFSFile:TestCreate')]),
+            'C2': ( testProgram, [('T1', 'MFSFile:TestUnlink')])
+        },
+        rendezvous = [('C1:T1:E2:block', 'C2:T1:E1:block'), 
+                      ('C1:T1:E3:block', 'C2:T1:E3:block')]
+    ))
+
+    env.addIntegrationTest(testfw.integration_test.IntegrationTest(
+        name = 'MFSFile:TestCreateUnlinkConcurrent2',
+        init_script = os.path.join(parent_dir, 'test/integration/mfs/init.sh'),
+        testfw = testProgram, server = serverProgram,
+        clients = { 
+            'C1': ( testProgram, [('T1', 'MFSFile:TestCreate')]),
+            'C2': ( testProgram, [('T1', 'MFSFile:TestUnlinkCreate')])
         },
         rendezvous = [('C1:T1:E2:block', 'C2:T1:E1:block'), 
                       ('C1:T1:E3:block', 'C2:T1:E3:block')]
